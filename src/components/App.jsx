49b421773d17385fd647224116483fb7d43dafec
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2023-12-29 14:45:22 +0100

    code refactoring with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ContactList from './ContactList';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const App = () => {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
-  handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+    setFilter(value);
   };
 
-  handleAddContact = (newContact) => {
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+  const handleAddContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
   };
 
-  handleDeleteContact = (id) => {
-    const updatedContacts = this.state.contacts.filter((contact) => contact.id !== id);
-    this.setState({
-      contacts: updatedContacts,
-    });
+  const handleDeleteContact = (id) => {
+    const updatedContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(updatedContacts);
   };
 
-  render() {
-    return (
-      <div>
-        <ContactForm onAddContact={this.handleAddContact}
-        contacts={this.state.contacts}  />
-        <Filter
-          filter={this.state.filter}
-          onInputChange={this.handleInputChange}
-        />
-        <ContactList
-          contacts={this.state.contacts}
-          filter={this.state.filter}
-          onDeleteContact={this.handleDeleteContact}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <ContactForm onAddContact={handleAddContact} contacts={contacts} />
+      <Filter filter={filter} onInputChange={handleInputChange} />
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        onDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,83 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import css from './ContactList.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-    submitted: false,
-  };
+const ContactForm = (props) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
-  handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
 
-  handleAddContact = (e) => {
+  const handleAddContact = (e) => {
     e.preventDefault();
-    this.setState({ submitted: true });
+    setSubmitted(true);
 
-    if (!this.state.name || !this.state.number) {
+    if (!name || !number) {
       alert('Please enter both name and phone number');
       return;
     }
 
-    if (this.props.contacts.some((contact) => contact.name === this.state.name)) {
-      alert(`${this.state.name} already exists in contacts and cannot be added.`);
+    if (props.contacts.some((contact) => contact.name === name)) {
+      alert(`${name} already exists in contacts and cannot be added.`);
       return;
     }
 
     const newContact = {
       id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
+      name: name,
+      number: number,
     };
 
-    this.props.onAddContact(newContact);
+    props.onAddContact(newContact);
 
-    this.setState({
-      name: '',
-      number: '',
-      submitted: false,
-    });
+    // Reset form state after adding contact
+    setName('');
+    setNumber('');
+    setSubmitted(false);
   };
 
-  render() {
-    return (
-      <form className={css.form}>
-        <label className={css.label}>
-          Name
-          <input
-            id={nanoid()}
-            className={`${css.input} ${this.state.submitted && !this.state.name && css.error}`}
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleInputChange}
-            required
-          />
-        </label>
-        <label>
-          Phone number
-          <input
-            id={nanoid()}
-            className={`${css.input} ${this.state.submitted && !this.state.number && css.error}`}
-            type="number"
-            name="number"
-            value={this.state.number}
-            onChange={this.handleInputChange}
-            required
-          />
-        </label>
-        <button className={css.addBtn} onClick={this.handleAddContact}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={css.form}>
+      <label className={css.label}>
+        Name
+        <input
+          id={nanoid()}
+          className={`${css.input} ${submitted && !name && css.error}`}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <label>
+        Phone number
+        <input
+          id={nanoid()}
+          className={`${css.input} ${submitted && !number && css.error}`}
+          type="number"
+          name="number"
+          value={number}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <button className={css.addBtn} onClick={handleAddContact}>
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default ContactForm;
